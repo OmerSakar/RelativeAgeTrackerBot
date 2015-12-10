a81643c2fd444ded8c33896480fbce315abe3972
Ömer Sakar <o.f.o.sakar@student.utwente.nl>
2015-12-10 20:12:32 +0100

    CRUD operations improved by returning data that can be used by the bot.

--- database/BirthDays.sql
+++ database/BirthDays.sql
@@ -1,25 +1,68 @@
 DROP TABLE IF EXISTS BirthDays CASCADE;
 CREATE TABLE BirthDays (
-  id                  SERIAL,
-  name                TEXT NOT NULL,
-  age            BIGINT,
-  PRIMARY KEY (id)
-
+  chatID        BIGINT,
+  full_name     TEXT NOT NULL,
+  age           BIGINT,
+  last_modified TEXT,
+  already_asked BOOLEAN,
+  PRIMARY KEY (full_name, chatID)
 );
 
-DROP FUNCTION IF EXISTS add_birthday(nameInput TEXT, ageInput BIGINT) CASCADE ;
-CREATE OR REPLACE FUNCTION add_BirthDay(nameInput TEXT, ageInput BIGINT)
+DROP FUNCTION IF EXISTS add_birthday(chat_idInput BIGINT, nameInput TEXT, ageInput BIGINT,
+                                     last_modifiedInput TEXT, already_askedInput BOOLEAN) CASCADE ;
+CREATE OR REPLACE FUNCTION add_BirthDay(chat_idInput BIGINT, nameInput TEXT, ageInput BIGINT,
+                                     last_modifiedInput TEXT, already_askedInput BOOLEAN)
     RETURNS void AS $$
     BEGIN
-      INSERT INTO BirthDays(name, age) VALUES (nameInput, ageInput);
+      INSERT INTO BirthDays(chatID, full_name, age, last_modified, already_asked)
+      VALUES (chat_idInput, nameInput, ageInput, last_modifiedInput, already_askedInput);
     END;
     $$ LANGUAGE plpgsql;
 
-DROP FUNCTION IF EXISTS get_birthday(nameInput TEXT) CASCADE ;
-CREATE OR REPLACE FUNCTION get_birthday(nameInput TEXT)
-    RETURNS TABLE(name TEXT, age BIGINT) AS $$
+DROP FUNCTION IF EXISTS get_birthday(nameInput TEXT, chat_idInput BIGINT) CASCADE ;
+CREATE OR REPLACE FUNCTION get_birthday(nameInput TEXT, chat_idInput BIGINT)
+    RETURNS TABLE(chat_id             BIGINT,
+                  name                TEXT,
+                  age                 BIGINT,
+                  last_modified       TEXT,
+                  already_asked       BOOLEAN) AS $$
     BEGIN
       RETURN QUERY
-        SELECT b.name, b.age FROM BirthDays b;
+        SELECT * FROM BirthDays
+        WHERE full_name = nameInput and
+              chatID = chat_idInput;
+    END;
+    $$ LANGUAGE plpgsql;
+
+DROP FUNCTION IF EXISTS update_birthday(nameInput TEXT, chat_idInput BIGINT, increment_by INT) CASCADE;
+CREATE OR REPLACE FUNCTION update_birthday(nameInput TEXT, chat_idInput BIGINT, increment_by INT)
+   RETURNS TABLE(_chat_id             BIGINT,
+                  _name                TEXT,
+                  _age                 BIGINT,
+                  _last_modified       TEXT,
+                  _already_asked       BOOLEAN) AS $$
+    BEGIN
+      UPDATE BirthDays
+        SET age = age + increment_by
+        WHERE full_name = nameInput and
+              chatID = chat_idInput;
+        RETURN QUERY SELECT * FROM get_birthday(nameInput, chat_idInput);
+    END;
+    $$ LANGUAGE plpgsql;
+
+
+DROP FUNCTION IF EXISTS remove_birthday(nameInput TEXT, chat_idInput BIGINT) CASCADE;
+CREATE OR REPLACE FUNCTION remove_birthday(nameInput TEXT, chat_idInput BIGINT)
+   RETURNS TABLE(_chat_id             BIGINT,
+                  _name                TEXT,
+                  _age                 BIGINT,
+                  _last_modified       TEXT,
+                  _already_asked       BOOLEAN) AS $$
+    BEGIN
+    RETURN QUERY SELECT * FROM get_birthday(nameInput, chat_idInput);
+      DELETE FROM BirthDays
+      WHERE full_name = nameInput and
+            chatID = chat_idInput;
+
     END;
     $$ LANGUAGE plpgsql;
--- database/fill_database.sql
+++ database/fill_database.sql
@@ -1,5 +1,13 @@
 
-SELECT add_birthday('name', 9);
-SELECT add_birthday('names', 10);
-SELECT add_birthday('namez', 2015);
-SELECT add_birthday('namesz', 2016);
+SELECT add_birthday(0,'omer', 9, 'text', FALSE);
+SELECT add_birthday(1,'jelle', 9, 'text', FALSE);
+SELECT add_birthday(2,'remco', 9, 'text', FALSE);
+SELECT add_birthday(3,'lukas', 9, 'text', FALSE);
+SELECT add_birthday(4,'melcher', 9, 'text', FALSE);
+SELECT add_birthday(5,'robin', 9, 'text', FALSE);
+SELECT add_birthday(6,'hans', 9, 'text', FALSE);
+SELECT add_birthday(7,'dylan', 9, 'text', FALSE);
+SELECT add_birthday(8,'heleen', 9, 'text', FALSE);
+SELECT add_birthday(9,'enes', 9, 'text', FALSE);
+SELECT add_birthday(10,'ismail', 9, 'text', FALSE);
+SELECT add_birthday(11,'tayfun', 9, 'text', FALSE);
